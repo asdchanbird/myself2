1642bde4b93e947fc2239b9ae470c752e141f16c
DESKTOP-VOQ8I9J\User <asdchanbird@gmail.com>
2023-01-07 21:52:58 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base: '/myself2/',
-=======
-  base: './myself2/',
->>>>>>> gh-pages
   plugins: [vue()],
 })
